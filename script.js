4a2d17ea08bde211cd4bd63e1c13531484da1a28
shekharlimbu <57363777+shekharlimbu@users.noreply.github.com>
2021-04-17 02:28:14 +0530

    Created script.js
    Each developer will add the assigned functionality.

---
+++ script.js
@@ -0,0 +1,18 @@
+//each developer is assigned with one functionality each corresponding to each of four buttons + - * and /
+//add code as assigned below
+
+function add(){
+
+}
+
+function subtract(){
+  
+}
+
+function multiply(){
+  
+}
+
+function divide(){
+  
+}
