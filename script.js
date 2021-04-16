a32621010873b59b40acbcd91249a32288b33f5d
shekharlimbu <57363777+shekharlimbu@users.noreply.github.com>
2021-04-17 04:32:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,7 @@ function add(){
     a=Number(document.getElementById("numA").value);
     b=Number(document.getElementById("numB").value);
     var c = a + b;
-    document.getElementById("operator").innerHTML = "+";
-    document.getElementById("result").innerHTML = c;
-    document.getElementById("msg").innerHTML = '';
+    
 }
 
 function subtract(){
