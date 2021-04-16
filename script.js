beae741f5c6b6e781c13a4c681c2cdcafcc4e36a
shekharlimbu <57363777+shekharlimbu@users.noreply.github.com>
2021-04-17 04:35:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
 //each developer is assigned with one functionality each corresponding to each of four buttons + - * and /
 //add code as assigned below
 
-function add(){
-    a=Number(document.getElementById("numA").value);
-    b=Number(document.getElementById("numB").value);
-    var c = a + b;
-    document.getElementById("operator").innerHTML = "+";
-    document.getElementById("result").innerHTML = c;
-    document.getElementById("msg").innerHTML = '';
-}
+
 
 function subtract(){
     a=Number(document.getElementById("numA").value);
