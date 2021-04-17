0361098e295c1de4973f753c6152237427629b85
shekhar-limbu-edu <shekhar.limbu.edu@gmail.com>
2021-04-17 11:28:08 +0530

    included divide functionality

--- script.js
+++ script.js
@@ -29,5 +29,16 @@ function multiply(){
 }
 
 function divide(){
-  
+    a=Number(document.getElementById("numA").value);
+    b=Number(document.getElementById("numB").value);
+    if(b!=0){
+        var c = a / b;
+        document.getElementById("operator").innerHTML = "/";
+        document.getElementById("result").innerHTML = c;
+    }
+    else{
+        document.getElementById("operator").innerHTML = "/";
+        document.getElementById("result").innerHTML = 'N/A';
+        document.getElementById("msg").innerHTML = '** Division by 0 is not allowed';
+    }
 }
